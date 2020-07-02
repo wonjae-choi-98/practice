52e6e1212b2c8c7d24691c7ebc8a4ff490485bf9
wonjae <cwj980119@gmail.com>
2020-07-02 16:47:43 +0900

    hi

---
+++ practice2741/practice2741/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; ++i) {
+		cout << i + 1 << '\n';
+	}
+}
